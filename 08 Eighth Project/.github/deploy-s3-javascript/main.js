5d7019a8aed00a67c0da015bbf9b283555e4786f
Alonso <alonvineba@gmail.com>
2024-10-07 08:36:05 +0200

    custom javacript action

--- 08 Eighth Project/.github/deploy-s3-javascript/main.js
+++ 08 Eighth Project/.github/deploy-s3-javascript/main.js
@@ -1,7 +1,11 @@
 console.log("Hello world from main")
 
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
 function run() {
-    console.log("")
+    core.notice('Hello from my custom javascript action!')
 }
 
 run();
